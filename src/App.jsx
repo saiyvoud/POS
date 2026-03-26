3508641a5749534cb694ee6c24c47805cb997bb1
beesaiyvoud <saiyvouds@gmail.com>
2026-03-26 18:31:56 +0700

    add

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-red-500'>Hello </div>
+  )
+}
+
+export default App
